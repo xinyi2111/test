23a6f11e1496f8957878e374a3ee5500c47c360a
xinyi2111 <74605345+xinyi2111@users.noreply.github.com>
2021-05-06 00:47:56 +0800

    Add files via upload

---
+++ Homework.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+//int main(){
+//	int a, b, c;
+//	printf("Please input three numbers.");
+//	scanf_s("%d%d%d", &a, &b, &c);
+//	if (a > b&&a > c){
+//		if (b > c){
+//			printf("%d>%d>%d", a, b, c);
+//		}
+//		else{
+//			printf("%d>%d>%d", a, c, b);
+//		}
+//	}
+//	if (b > a&&b > c){
+//		if (a > c){
+//			printf("%d>%d>%d", b, a, c);
+//		}
+//		else{
+//			printf("%d>%d>%d", b, c, a);
+//		}
+//	}
+//	if (c > b&&c > a){
+//		if (b > a){
+//			printf("%d>%d>%d", c, b, a);
+//		}
+//		else{
+//			printf("%d>%d>%d", c, a, b);
+//		}
+//	}
+//	return 0;
+//
+//}
+//int main()
+//{
+//	int i = 1;
+//	for (i = 1; i <= 100; i++){
+//		if (i % 3 == 0){
+//			printf("%d\n", i);
+//		}
+//	}
+//}
+//int main()
+//{
+//	int a, b;
+//	scanf_s("%d%d", &a, &b);
+//	printf("Please input two numbers.\n");
+//	int c;
+//	if (c == 1){
+//		if (a > b){
+//			printf("最大公约数为%d", b);
+//		}
+//		else{
+//			printf("最大公约数为%d", a);
+//		}
+//	}
+//	else{
+//		if (a%c == 0 && b%c == 0){
+//			printf("最大公约数为%d", c);
+//		}
+//	}
+//}
