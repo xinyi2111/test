618dc4e45c17249e3ac3c69454bb39c12b41b1ed
xinyi2111 <74605345+xinyi2111@users.noreply.github.com>
2021-05-07 00:08:56 +0800

    Add files via upload

---
+++ practice.c
@@ -0,0 +1,53 @@
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int i;
+//	int a, b, c;
+//	for (i = 100; i<1000; i++)
+//	{
+//		a = i % 10;
+//		b = i / 10 % 10;
+//		c = i / 100;
+//
+//		if (i == (a*a*a + b*b*b + c*c*c))
+//		{
+//			printf("%d\n", i);
+//		}
+//
+//	}
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//
+//	int i = 0,j = 0, n = 0;
+//	for (i = 0; i < 7; i++)
+//	{
+//		for (n = 0; n < 6 - i; n++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 0; j < 2 * i + 1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	for (i = 0; i < 6; i++)
+//	{
+//		for (n = 0; n < i + 1; n++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 0; j < 2 * (6 - i) - 1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
