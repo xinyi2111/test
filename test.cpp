026bd850d4044442b277db78ba299e037b9dfbbd
xinyi2111 <74605345+xinyi2111@users.noreply.github.com>
2021-05-26 23:18:59 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,35 +1,22 @@
 #include<iostream>
+#include<sstream>
 using namespace std;
 
-class String
+
+
+class Base
 {
-	//Ç³¿½±´
 public:
-	String(const char *str= "")
-	{
-		m_data = new char[strlen(str) + 1];
-		strcpy(m_data, str);
-	}
-	String& operator=(const String &s)
-	{
-		if (this != &s)
-		{
-			String tmp = s;
-			std::swap(m_data, tmp.m_data);
-		}
-		return *this;
-	}
-	~String()
-	{}
+
 
 
 private:
-	char*m_data;
+
+
+
 };
 
-
-
-
+//Í¬ÃûÒþ²Ø
 void main()
 {
 
