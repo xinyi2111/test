b3209650fba3b7ea0ff83958b66ccff1195e65fe
xinyi2111 <74605345+xinyi2111@users.noreply.github.com>
2021-06-17 23:56:49 +0800

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -1,350 +1,154 @@
 #include<iostream>
+#include<string>
+#include<set> //集合
+#include<map> //映射
 #include<vector>
+#include<functional>
 using namespace std;
-
-typedef enum{RED = 0, BLACK=1}Color_Type;
-
-template<class Type>
-class RBTree;
-//红黑树
-template<class Type>
-class RBNode
-{
-	friend class RBTree<Type>;
-public:
-	RBNode(Type d):data(d), leftChild(nullptr), rightChild(nullptr),parent(nullptr),color(RED)
-	{}
-	~RBNode()
-	{}
-private:
-	Type data;
-	RBNode *leftChild;
-	RBNode *rightChild;
-	RBNode *parent;
-	Color_Type color;
-};
-
-template<class Type>
-class RBTree
-{
-public:
-	RBTree() :root(Nil), Nil(_Buynode())
-	{
-		Nil->leftChild = Nil->rightChild = Nil->parent = nullptr;
-		Nil->color = BLACK;
-	}
-public:
-	//bool Insert(const Type &x)
-	//{
-	//	return Insert(root, x);
-	//}
-	bool Remove(const Type &key)
-	{
-		return Remove(root, key);
-	}
-protected:
-	/*bool Insert(RBNode<Type> *&t, const Type &x);*/
-	bool Remove(RBNode<Type> *&t, const Type &key);
-	RBNode<Type>* _Buynode(const Type &x = Type())
-	{
-		RBNode<Type> *s = new RBNode<Type>(x);
-		s->leftChild = s->rightChild = s->parent = Nil;
-		return s;
-	}
-protected:
-	/*void Insert_Fixup(RBNode<Type> *&t, RBNode<Type> *x);*/
-	void Remove_Fixup(RBNode<Type> *&t, RBNode<Type> *x);
-protected:
-	void RotateRight(RBNode<Type> *&t, RBNode<Type> *x)//右转方法
-	{
-		RBNode<Type> *y = x->leftChild;
-		x->leftChild = y->rightChild;
-		if(y->rightChild != Nil)
-			y->rightChild->parent = x;
-
-		y->rightChild = x;
-		if(x == t)
-			t = y;
-		else if(x == x->parent->leftChild)
-			x->parent->leftChild = y;
-		else
-			x->parent->rightChild = y;
-
-		y->parent = x->parent;
-		x->parent = y;
-	}
-	void RotateLeft(RBNode<Type> *&t, RBNode<Type> *x)//左转方法
-	{
-		RBNode<Type> *y = x->rightChild;
-		x->rightChild = y->leftChild;
-		if(y->leftChild != Nil)
-			y->leftChild->parent = x;
-
-		y->leftChild = x;
-		if(x == t)
-			t = y;
-		else if(x == x->parent->leftChild)
-			x->parent->leftChild = y;
-		else
-			x->parent->rightChild = y;
-
-		y->parent = x->parent;
-		x->parent = y;
-	}
-private:
-	RBNode<Type> *Nil;
-	RBNode<Type> *root;
-};
-
-//template<class Type>
-//bool RBTree<Type>::Insert(RBNode<Type> *&t, const Type &x)
-//{
-//	//1 按照bst进行节点插入
-//	RBNode<Type> *pr = Nil;
-//	RBNode<Type> *p = t;
-//	while(p != Nil)
-//	{
-//		if(x == p->data)
-//			return false;
-//
-//		pr = p;
-//
-//		if(x < p->data)
-//			p = p->leftChild;
-//		else
-//			p = p->rightChild;
-//	}
-//
-//	p = _Buynode(x);
-//
-//	if(pr == Nil)
-//		t = p;
-//	else
-//	{
-//		if(x < pr->data)
-//			pr->leftChild = p;
-//		else
-//			pr->rightChild = p;
-//		p->parent = pr;
-//	}
-//
-//	//2 调整平衡
-//	Insert_Fixup(t, p);
-//
-//	return true;
-//}
-//
-//template<class Type>
-//void RBTree<Type>::Insert_Fixup(RBNode<Type> *&t, RBNode<Type> *x)
-//{
-//	while(x->parent->color == RED)
-//	{
-//		RBNode<Type> *s;
-//		if(x->parent == x->parent->parent->leftChild) 
-//		{
-//			//左分支
-//			s = x->parent->parent->rightChild;
-//
-//			if(s->color == RED)
-//			{
-//				//状况三
-//				x->parent->color = BLACK;
-//				s->color = BLACK;
-//				x->parent->parent->color = RED;
-//				x = x->parent->parent;
-//			}
-//			else
-//			{
-//				if(x == x->parent->rightChild)
-//				{
-//					//状况二
-//					x = x->parent;
-//					RotateLeft(t, x);
-//				}
-//				//状况一
-//				x->parent->color = BLACK;
-//				x->parent->parent->color = RED;
-//				RotateRight(t, x->parent->parent);
-//			}
-//		}
-//		else
-//		{
-//			//右分支
-//			s = x->parent->parent->leftChild;
-//			if(s->color == RED)
-//			{
-//				//状况三
-//				x->parent->color = BLACK;
-//				s->color = BLACK;
-//				x->parent->parent->color = RED;
-//				x = x->parent->parent;
-//			}
-//			else
-//			{
-//				if(x == x->parent->leftChild)
-//				{
-//					//状况二
-//					x = x->parent;
-//					RotateRight(t, x);
-//				}
-//				//状况一
-//				x->parent->color = BLACK;
-//				x->parent->parent->color = RED;
-//				RotateLeft(t, x->parent->parent);
-//			}
-//		}
-//	}
-//	t->color = BLACK;
-//}
-
-template<class Type>
-bool RBTree<Type>::Remove(RBNode<Type> *&t, const Type &key)
-{
-	//1 按照BST的规则删除节点
-	RBNode<Type> *p = t, *q;
-	while(p != Nil)//查找节点
-	{
-		if(key == p->data)
-			break;
-		if(key < p->data)
-			p = p->leftChild;
-		else
-			p = p->rightChild;
-	}
-	if(p == Nil)//要查找的节点不存在
-		return false;
-
-	if(p->leftChild!=Nil && p->rightChild!=Nil)//要删除的节点有左右子树
-	{
-		q = p->leftChild;//寻找前驱
-		while(q->rightChild != Nil)
-			q = q->rightChild;
-
-		p->data = q->data;
-		p = q;
-	}
-
-	if(p->leftChild != Nil)
-		q = p->leftChild;
-	else
-		q = p->rightChild;
-
-
-	if(p == p->parent->leftChild)
-		p->parent->leftChild = q;
-	else
-		p->parent->rightChild = q;
-
-	//if(q != Nil)
-	q->parent = p->parent;
-
-	//2 调整平衡
-	if(p->color == BLACK)
-		Remove_Fixup(t, q);
-
-	//删除节点
-	delete p;
-	return true;
-}
-
-template<class Type>
-void RBTree<Type>::Remove_Fixup(RBNode<Type> *&t, RBNode<Type> *x)//删除完节点调整平衡
-{
-	RBNode<Type> *w; //兄弟节点
-	while(x!=t && x->color==BLACK)
-	{
-		if(x == x->parent->leftChild) 
-		{
-			//删除的节点在左分支
-			w = x->parent->rightChild;
-
-			//情形四
-			if(w->color == RED)
-			{
-				w->color = BLACK;
-				x->parent->color = RED;
-				RotateLeft(t, x->parent);
-				w = x->parent->rightChild;
-			}
-			//情形三
-			if(w->leftChild==Nil && w->rightChild==Nil)
-			{
-				x->parent->color = BLACK;
-				w->color = RED;
-				x = x->parent;
-			}
-			else
-			{
-				//情形二
-				if(w->leftChild != Nil)
-				{
-					w->color = RED;
-					w->leftChild->color = BLACK;
-					w = w->leftChild;
-					RotateRight(t, w->parent);
-				}
-				//情形一
-				w->color = x->parent->color;
-				x->parent->color = BLACK;
-				w->rightChild->color = BLACK;
-				RotateLeft(t, x->parent);
-				x = t;
-			}
-		}
-		else
-		{
-			//删除的节点在右分支
-			w = x->parent->leftChild;
-
-			//情形四
-			if(w->color == RED)
-			{
-				w->color = BLACK;
-				x->parent->color = RED;
-				RotateRight(t, x->parent);
-				w = x->parent->leftChild;
-			}
-
-			//情形三
-			if(w->leftChild==Nil && w->rightChild==Nil)
-			{
-				w->color = RED;
-				x->parent->color = BLACK;
-				x = x->parent;
-			}
-			else
-			{
-				//情形二
-				if (w->rightChild != Nil)
-				{
-					w->color = RED;
-					w->rightChild->color = BLACK;
-					w = w->rightChild;
-					RotateLeft(t, w->parent);
-				}
-				//情形一
-				w->color = x->parent->color;
-				x->parent->color = BLACK;
-				w->leftChild->color = BLACK;
-				RotateRight(t, x->parent);
-				x = t;
-			}
-		}
-	}
-	x->color = BLACK;//被删除的节点只有一个红色子节点
-}
 
 void main()
 {
-	vector<int> iv = {10, 7, 8, 15, 5, 6, 11, 13, 12}; 
-	RBTree<int> rb;
+	pair<int, string> v[] = 
+	{{3,"abc"}, {2,"xyz"},{1,"lmn"},{8,"opq"},{5,"asd"},
+	{2,"xyz"},{1,"lmn"},{2,"xyz"},{1,"lmn"},{2,"xyz"},{1,"lmn"}};
 
-	for(const auto &e : iv)
-		rb.Insert(e);
+	int n = sizeof(v) / sizeof(v[0]);
 
-	rb.Remove(5);
-	rb.Remove(7);
-	rb.Remove(6);
+	multimap<int, string> mp;
+	for(int i=0; i<n; ++i)
+		mp.insert(v[i]);
 
-	return;
+	//mp[30] = "ABC";
+
+	for(const auto &e : mp)
+		cout<<e.first<<" : "<<e.second<<endl;
 }
+
+/*
+void main()
+{
+	pair<int, string> v[] = {{3,"abc"}, {2,"xyz"},{1,"lmn"},{8,"opq"},{5,"asd"}};
+	int n = sizeof(v) / sizeof(v[0]);
+
+	map<int, string> mp;
+	for(int i=0; i<n; ++i)
+		mp.insert(v[i]);
+
+	map<int,string>::iterator it = mp.begin();
+	while(it != mp.end())
+	{
+		cout<<it->first<<" : "<<it->second<<endl;
+		++it;
+	}
+
+	for(const auto &e : mp)
+		cout<<e.first<<" : "<<e.second<<endl;
+}
+
+/*
+void main()
+{
+	int ar[] = {10, 7, 8, 15, 5, 6, 15,15,15,15,15,15,11, 13, 12};
+	int n = sizeof(ar) / sizeof(ar[0]);
+	
+	set<int> myset(ar, ar+n);
+
+	cout<<"15 count = "<<myset.count(15)<<endl;
+
+	for(const auto &e : myset)
+		cout<<e<<" ";
+	cout<<endl;
+}
+
+/*
+void main()
+{
+	int ar[] = {10, 7, 8, 15, 5, 6, 11, 13, 12};
+	int n = sizeof(ar) / sizeof(ar[0]);
+	vector<int> iv = {10, 7, 8, 15, 5, 6, 11, 13, 12};
+	//set<int> myset(iv.begin(), iv.end());
+	set<int> myset(ar, ar+n);
+	cout<<"size = "<<myset.size()<<endl;
+
+	set<int>::iterator it = myset.begin();
+	while(it != myset.end())
+	{
+		cout<<*it<<" ";
+		++it;
+	}
+	cout<<endl;
+
+	auto it1 = myset.begin();
+	while(it1 != myset.end())
+	{
+		cout<<*it1<<" ";
+		++it1;
+	}
+	cout<<endl;
+
+	for(const auto &e : myset)
+		cout<<e<<" ";
+	cout<<endl;
+}
+
+/*
+void main()
+{
+	vector<int> iv = {10, 7, 8, 15, 5, 6, 11, 13, 12};
+	set<int> myset;
+	//set<int, greater<int>> myset;
+	for(const auto &e : iv)
+		myset.insert(e);
+
+	set<int>::iterator it = myset.begin();
+	while(it != myset.end())
+	{
+		cout<<*it<<" ";
+		++it;
+	}
+	cout<<endl;
+
+	auto it1 = myset.begin();
+	while(it1 != myset.end())
+	{
+		cout<<*it1<<" ";
+		++it1;
+	}
+	cout<<endl;
+
+	for(const auto &e : myset)
+		cout<<e<<" ";
+	cout<<endl;
+}
+
+/*
+//值对结构 pair
+
+void main()
+{
+	pair<int, string> p1 = make_pair<int,string>(1,"abc");
+}
+
+/*
+/*
+template<class _T1, class _T2> 
+struct pair 
+{
+	typedef _T1 first_type;
+	typedef _T2 second_type;
+
+	pair(): first(_T1()), second(_T2()) 
+	{}
+	pair(const _T1& _V1, const _T2& _V2)
+		: first(_V1), second(_V2) {}
+
+	_T1 first;
+	_T2 second;
+};
+
+
+void main()
+{
+	pair<int, string> p1{1, "abc"};
+	cout<<p1.first<<" : "<<p1.second<<endl;
+}*/
