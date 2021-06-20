c79a8f0e5b367e225fa20ee130c9e0d1a72a7ac1
xinyi2111 <74605345+xinyi2111@users.noreply.github.com>
2021-06-20 20:45:49 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,12 +1,46 @@
 #include<iostream>
-#include<string.h>
-#include<sdoias.h>
 using namespace std;
-v
+
+class String
+{
+	//浅拷贝
+public:
+	String(const char *str= "")
+	{
+		m_data = new char[strlen(str) + 1];
+		strcpy(m_data, str);
+	}
+	String(const String &str)
+	{
+		m_data = new char[strlen(s.m_data)+1];
+		strcpy(m_data, s.m_data);
+	}
+	String& operator=(const String &s)
+	{
+		if (this != &s)
+		{
+			String tmp = s;
+			std::swap(m_data, tmp.m_data);
+		}
+		return *this;
+	}
+	 
+	String()
+	{
+
+	}
+	~String()
+	{}
+
+	//浅拷贝会带来同一空间被多次释放的问题
+
+private:
+	char*m_data;
+};
 
 
-oid main()
+
+
+void main()
 {
 
-
-}
