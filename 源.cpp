3012dd5b19fa1f078dc850e06f544d8156109bb5
xinyi2111 <74605345+xinyi2111@users.noreply.github.com>
2021-06-26 01:18:14 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,57 +1,115 @@
-#include "common.h"
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<assert.h>
+using namespace std;
 
-int main() {
-	//管理类采用单例模式
-	manager* manag = manager::simpleInstance();
-	vector<student*> students;
-	manag->start(students);
-	int input = 0;
-	string name;
-	int up = 0, down = 0;
-	manag->menu();
-	while (cin >> input && input != 0) {
-		switch (input) {
-		case 1:
-			manag->titleMenu();
-			manag->printScore<vector<student*>, student*>(students);
-			break;
-		case 2:
-			cout << "按平均分排序并显示" << endl;
-			cout << "计算平均分成功" << endl;
-			if (manag->sortByAverageScore<vector<student*>, student*>(students))
-				cout << "排序成功" << endl << endl;
-			else
-				cout << "发生未知错误" << endl << endl;
-			manag->AveragetitleMenu();
-			manag->printAverangeScore<vector<student*>, student*>(students);
-			break;
-		case 3:
-			cout << "各科的班平均成绩" << endl;
-			manag->titleMenuOfClassAverage();
-			manag->calculateAverageOfScoreAndPrint(students);
-			break;
-		case 4:
-			cout << "请输入要查询的学生的姓名：" << endl;
-			cin >> name;
-			manag->searchByName(students, name);
-			break;
-		case 5:
-			cout << "显示所有不及格的学生:" << endl;
-			manag->printScoreFailure(students);
-			break;
-		case 6:
-			cout << "显示最高和最低成绩" << endl;
-			manag->findMaxAndMin(students);
-			break;
-		case 7:
-			cout << "请输入成绩区间的两个数字（闭区间）" << endl;
-			cin >> up >> down;
-			manag->coutAndSegmentScore(students, up, down);
-			break;
-		default:
-			cout << "输入错误，请重新输入：" << endl;
-		}
-		manag->menu();
-	}	manag->ending(students);
-	return 0;
-}
+//AVL
+template<class Type>
+class AVLTree;
+
+//节点类
+template<class Type>
+class AVLNode
+{
+	friend class AVLTree<Type>;
+public:
+	AVLNode() :data(Type()), leftChild(nullptr), rightChild(nullptr), bf(0)
+	{}
+	AVLNode(Type d) :data(d), leftChild(nullptr), rightChild(nullptr), bf(0)
+	{}
+	~AVLNode()
+	{}
+private:
+	Type data;
+	AVLNode *leftChild;
+	AVLNode *rightChild;
+	int bf;  //平衡因子 0 -1 1  2 -2
+};
+
+template<class Type>
+class AVLTree
+{
+public:
+	AVLTree() : root(nullptr)
+	{}
+public:
+	bool Insert(const Type &x)
+	{
+		return Insert(root, x);
+	}
+	bool Remove(const Type &key)
+	{
+		return Remove(root, key);
+	}
+protected:
+	bool Insert(AVLNode<Type> *&t, const Type &x);
+	bool Remove(AVLNode<Type> *&t, const Type &key);
+protected:
+	void RotateR(AVLNode<Type> *&ptr)
+	{
+		AVLNode<Type> *subR = ptr;
+		ptr = subR->leftChild;
+		subR->leftChild = ptr->rightChild;
+		ptr->rightChild = subR;
+		ptr->bf = subR->bf = 0;
+	}
+	void RotateL(AVLNode<Type> *&ptr)
+	{
+		AVLNode<Type> *subL = ptr;
+		ptr = subL->rightChild;
+		subL->rightChild = ptr->leftChild;
+		ptr->leftChild = subL;
+		ptr->bf = subL->bf = 0;
+	}
+	void RotateLR(AVLNode<Type> *&ptr)
+	{
+		AVLNode<Type> *subR = ptr;
+		AVLNode<Type> *subL = ptr->leftChild;
+		ptr = subL->rightChild;
+
+		subL->rightChild = ptr->leftChild;
+		ptr->leftChild = subL;
+		//subL->bf
+		if (ptr->bf <= 0)
+			subL->bf = 0;
+		else
+			subL->bf = -1;
+
+		subR->leftChild = ptr->rightChild;
+		ptr->rightChild = subR;
+		//subR->bf;
+		if (ptr->bf >= 0)
+			subR->bf = 0;
+		else
+			subR->bf = 1;
+
+		ptr->bf = 0;
+	}
+	void RotateRL(AVLNode<Type> *&ptr)
+	{
+		AVLNode<Type> *subL = ptr;
+		AVLNode<Type> *subR = ptr->rightChild;
+		ptr = subR->leftChild;
+
+		subR->leftChild = ptr->rightChild;
+		ptr->rightChild = subR;
+		//subR->bf
+		if (ptr->bf >= 0)
+			subR->bf = 0;
+		else
+			subR->bf = 1;
+
+		subL->rightChild = ptr->leftChild;
+		ptr->leftChild = subL;
+		//subL->bf
+		if (ptr->bf <= 0)
+			subL->bf = 0;
+		else
+			subL->bf = -1;
+
+		ptr->bf = 0;
+	}
+private:
+	AVLNode<Type> *root;
+};
