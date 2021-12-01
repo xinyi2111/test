ed5d1d0733ca1bda33fc1bef7798d990bc3b6c5b
xinyi2111 <74605345+xinyi2111@users.noreply.github.com>
2021-12-01 21:00:53 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,38 +1,99 @@
 #include<stdio.h>
 //int main()
 //{
-//	int a = 0x11223344;//一个16进制位是4个二进制位
-//	/*int d = 1;*/
-//	int* pa = &a;
-//	//char* pc = &a;
-//	//char* pc = &a;
-//	/*int* c = &d;*/
-//	//*pa = 0;
-//	//*pc = 0;
-//	printf("%p\n", pa);
-//	printf("%p\n", pa + 1);
-//	//printf("%c\n", c);
+//	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+//	int* p = arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", *p);
+//		p++;
+//	}
 //	return 0;
-//}
 //
+//}
+//int* test()
+//{
+//	int a = 10;
+//	return &a;
+//}
 //int main()
 //{
-//	int arr1[10] = { 0 };
-//	int* p = arr1;//数组名等于首元素地址
-//	//指针变量p存放数组第一个元素地址
-//	return 0;
-
+//	int* p = test();
+//	printf("%d\n", *p);
+////	return 0;
+////}
+//
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		str++;
+//		count++;
+//	}
+//	return count;
+//}
 //int main()
 //{
-//	int* p;
-//	*p = 20;
+//	char arr1[] = "a,b,c,d,f";
+//	int len = my_strlen(arr1);
+//	printf("%d\n", len);
 //	return 0;
 //}
 
+//利用指针-指针求字符串长度
+
+//int my_strlen(char* str)
+//{
+//	char* start = str;
+//	while (*str != '\0')
+//	{
+//		str++;
+//	}
+//	return str - start;
+//
+//}
+//int main()
+//{
+//	char arr2[] = "asdfg";
+//	int len = my_strlen(arr2);
+//	printf("%d\n", len);
+//	return 0;
+//}
+
+//int main()
+//{
+//
+//	int arr[3][5] = {0}；
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int* p = &a;
+//	int** pp = &p;//二级指针
+//	return 0;
+//}
+
+//指针数组，存放指针的数组
 int main()
 {
-	int a = 0x11223344;
-	int* p = &a;
-	*p = 0;
+	int a = 10;
+	int b = 2;
+	int c = 3;
+	int* arr[3] = {&a,&b,&c};//存放整型指针的数组
+	char* p = "abcdef";
+	printf("%s\n", p);
+	char* arr1[] = { "abc", "hello", "world" };
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		printf("%s\n",arr1[i]);
+
+	}
+	
+	//字符指针数组
 	return 0;
 }
