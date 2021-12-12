70dc11d91ddaf8d159e967cbadfbcef578512c7e
xinyi2111 <74605345+xinyi2111@users.noreply.github.com>
2021-12-12 19:51:03 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,82 +1,38 @@
-//#include<stdio.h>
-//int main()
-//{
-//	int a = 4;
-//	printf("%d\n", ++a);//前置++先使用，后打印
-//	printf("%d\n", a++);//后置++
-//	int b = (int)2.14;
-//	return 0;
-//}
-
-//#include<stdio.h>
-//
-//void test1(int arr1[])
-//{
-//	printf("%d\n", sizeof(arr1));
-//}
-//void test2(char arr2[])
-//{
-//	printf("%d\n", sizeof(arr2));
-//
-//}
-//int main()
-//{
-//	int arr1[10] = { 0 };
-//	char arr2[10] = { 0 };
-//	printf("%d\n", sizeof(arr1));
-//	printf("%d\n", sizeof(arr2));
-//	test1(arr1);
-//	test2(arr2);
-//	return 0;
-//}
-//
-//#include<stdio.h>
-//int main()
-//{
-//	int i = 0, a = 0, b = 2, c = 3, d = 4;
-//	i = a++ && ++b && d++;
-//	printf("%d %d %d %d ", a, b, c, d);
-//	return 0;
-//	//int i = 0, a = 1, b = 2, c = 3, d = 4;
-//	//i = a++ && ++b && d++;
-//	//printf("%d %d %d %d ", a, b, c, d);
-//	//return 0;
-//	//int i = 0, a = 1, b = 2, c = 3, d = 4;
-//	//i = a++ || ++b || d++;
-//	//printf("%d %d %d %d ", a, b, c, d);
-//	//return 0;
-//
-//}
-
-//#include<stdio.h>
-//int main()
-//{
-//	int a = 1;
-//	int b = 0;
-//	int max = 0;
-//	if (a > 5)
-//		b = 3;
-//	else
-//	    b = -3;
-//	b = (a > 5 ? 3 : -3);
-//	max = (a > b ? a:b);
-//	printf("%d\n", max);
-//	return 0;
-//}
-
 #include<stdio.h>
-struct s
-{
-	char name[20];
-	int age;
-	char number[10];
-};
+//枚举关键——enum
+
 int main()
 {
-	int a = 10;
-	struct s s1 = { "小明",21, "19310532" };
-	struct s* p = &s1;
-	printf("%s\n", (*p).name);
-	printf("%s\n", p->name);
+	char arr1[] = "abc";
+	char arr2[] = {'a','b','c',0};//'\0'也是字符，是字符串结束的标志，不过'\0'本身的值就是0
+	printf("%s\n", arr2);
+	printf("%s\n", arr1);
+
 	return 0;
 }
+//enum Sex
+//{
+//	MALE,//这些是枚举常量
+//	FEMALE,
+//	SECRET
+//};
+//int main()
+//{
+//	enum Sex s = MALE;
+//	s = FEMALE;
+//	printf("%d\n",s);
+//	printf("%d\n", MALE);//0
+//	printf("%d\n",FEMALE);//1
+//	printf("%d\n", SECRET);//2
+//}
+
+////#define 定义的标识符常量
+//#define MAX 10
+//int main()
+//{
+//	const int sr = 5;
+//	printf("%d",sr);
+//	//sr = 6;//本质还是变量，只不过具有常属性
+//	//printf("%d", sr);
+//	return 0;
+//}
