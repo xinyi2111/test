6c6cafdfb295538465c0cbe1d6a9e9ce6bfceb1c
xinyi2111 <74605345+xinyi2111@users.noreply.github.com>
2021-12-13 22:44:39 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -1,38 +1,10 @@
-#include<stdio.h>
-//枚举关键——enum
-
+#include<iostream>
+using namespace std;
 int main()
 {
 	char arr1[] = "abc";
-	char arr2[] = {'a','b','c',0};//'\0'也是字符，是字符串结束的标志，不过'\0'本身的值就是0
-	printf("%s\n", arr2);
-	printf("%s\n", arr1);
-
+	char arr2[] = {'a','b','c'};
+	printf("%d\n", strlen(arr1));
+	printf("%d\n", strlen(arr2));
 	return 0;
 }
-//enum Sex
-//{
-//	MALE,//这些是枚举常量
-//	FEMALE,
-//	SECRET
-//};
-//int main()
-//{
-//	enum Sex s = MALE;
-//	s = FEMALE;
-//	printf("%d\n",s);
-//	printf("%d\n", MALE);//0
-//	printf("%d\n",FEMALE);//1
-//	printf("%d\n", SECRET);//2
-//}
-
-////#define 定义的标识符常量
-//#define MAX 10
-//int main()
-//{
-//	const int sr = 5;
-//	printf("%d",sr);
-//	//sr = 6;//本质还是变量，只不过具有常属性
-//	//printf("%d", sr);
-//	return 0;
-//}
